9aeb903e639e585be7824694fac45c9a9da29466
pydave <pydave@gmail.com>
2009-03-22 20:41:23 -0700

    Improve docstrings for main and add help option.

--- main.py
+++ main.py
@@ -1,19 +1,30 @@
+#! /usr/bin/env python
+
+"""
+Walks a directory tree and performs an action when a trigger is hit.
+
+Currently setup to build or clean ctags info from a packages or basekit folder.
+
+Usage: %s path-to-root
+Example: %s ~/code/someproject
+"""
+
+import sys
+
 import deepdir
 
 
 def _usage():
-    print \
-"""
-    Usage: %s path-to-root
-    Example: %s ~/code/someproject
-"""
+    print __doc__
 
 
 def main():
     import sys
     try:
         rootpath = sys.argv[1]
-    except IndexError:
+        if rootpath == '--help':
+            raise ValueError()
+    except (IndexError, ValueError):
         _usage()
         return
 
